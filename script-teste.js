0a2e4bd006f40b1e5b59b6c8f990788e3b2aceef
DarlanLS2 <Darlanleite.arnaldo@gmail.com>
2026-05-25 21:02:51 -0300

    safe point

--- script-teste.js
+++ script-teste.js
@@ -11,12 +11,13 @@
 (function () {
   'use strict';
 
+  // Procurar por todos os spans esta deixando lento pae
   function highlight() {
     const spans = document.querySelectorAll("span");
 
     spans.forEach(span => {
       const text = span.textContent.trim();
-      const regex_estagio = /Estágio|Estagio|Estagiário|Estagiario/i;
+      const regex_estagio = /Estágio|Estagio|Estagiário|Estagiario|Estagiária|Estagiaria/i;
       const regex_assistente = /Assistente/i;
       
       if (text.length > 150) {
@@ -30,22 +31,118 @@
       }
     });
 
-    const mainDiv = document.querySelector("[componentkey='SearchResultsMainContent']");
-
-    mainDiv.querySelectorAll("hr").forEach(hr => {
-      hr.style.setProperty("display", "none", "important");
-    })
-
-    // console.log(mainDiv.querySelectorAll(":scope > *"))
   }
 
+  var list = []
+
+  function t(index, spans) {
+    spans.forEach(span => {
+      const text = span.textContent.trim();
+      const numero = text.match(/[0-9]+/);
+      const hourRegex = /hora|horas/i
+
+      if (hourRegex.test(text)) {
+        list.push({
+          index : index,
+          hr : Number(numero[0])
+        })
+      }
+    })
+
+  }
+  function getJobsSortedByHour() {
+
+  }
   
-  highlight();
+  function getJobsDiv() {
+    return mainDiv = document.querySelector("[componentkey='SearchResultsMainContent']");
+  }
+
+  function getJobsDivClone() {
+    
+  }
+
+  function divSort() {
+    list = [];
+
+    const mainDiv = getJobsDiv();
+
+    mainDiv.style.display = "none";
+
+    const oldClone = document.querySelector("#clone");
+
+    if (oldClone) {
+      oldClone.remove();
+    }
+
+    const mainDivClone = mainDiv.cloneNode(true);
+
+    mainDivClone.id = "clone"
+    
+    if (mainDivClone.style.display != "flex") {
+      mainDivClone.style.display = "flex";
+    }
+
+    mainDiv.parentNode.appendChild(mainDivClone)
+
+    const elements = mainDivClone.querySelectorAll(":scope > *");
+
+    const hr = elements[1]
+
+    elements.forEach((element, index) => {
+      const spans = element.querySelectorAll("span[aria-hidden='true']")
+
+      spans.forEach(span => {
+        const text = span.textContent.trim();
+        const numero = text.match(/[0-9]+/);
+        const hourRegex = /hora|horas/i
+
+        if (hourRegex.test(text)) {
+          list.push({
+            index : index,
+            hr : Number(numero[0])
+          })
+        }
+      })
+    })
+
+    list.sort((a, b) => a.hr - b.hr)
+
+    const jobsSortedByHour = []
+
+    list.forEach(element => {
+      jobsSortedByHour.push(elements[el.index])
+    })
+
+    mainDivClone.innerHTML = "";
+
+    observer.disconnect();
+
+    jobsSortedByHour.forEach(el => {
+      mainDivClone.appendChild(el)
+      mainDivClone.appendChild(hr)
+    })
+
+
+
+    observer.observe(document.body, { childList: true, subtree: true });
+
+    // mainDivClone.querySelectorAll("hr").forEach(hr => {
+    //   hr.style.setProperty("display", "none", "important");
+    // })
+    //
+    // console.log(mainDivClone.querySelectorAll(":scope > *"))
+  }
 
   // Cria observador que vigia o DOM e roda uma função quando algo muda
   const observer = new MutationObserver(() => {
-    highlight();
+    // highlight();
+    divSort()
   });
+  
+  // highlight();
+  divSort();
+
 
   observer.observe(document.body, { // Starta observação do body
     childList: true, // Detecta elementos adicionados ou removidos
